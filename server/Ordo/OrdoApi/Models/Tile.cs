4fec2c682f5a3369971397a3e199833272414496
Holmberg <48940210+Holmbeerg@users.noreply.github.com>
2026-02-27 23:30:12 +0100

    refactor(api): push GameStateDto directly to clients on update
    - eliminates a round trip

--- server/Ordo/OrdoApi/Models/Tile.cs
+++ server/Ordo/OrdoApi/Models/Tile.cs
@@ -2,6 +2,7 @@ namespace OrdoApi.Models;
 
 public class Tile
 {
+    public string Id { get; set; } = Guid.NewGuid().ToString(); // for frontend
     public char Letter { get; set; }
     public int Value { get; set; }
     public bool IsBlank { get; set; }
